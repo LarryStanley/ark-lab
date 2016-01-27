4a872d964fd21ee166c5e2a0977fe6daa542a637
LarryStanley <kill.xmurderer@gmail.com>
2016-01-27 10:51:25 +0800

    New login page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,53 @@
-<form method="POST" action="/auth/login">
-    {!! csrf_field() !!}
+@extends("default")
+@section("head")
+    <style>
+        html,body {
+            background-color: #F44336;
+        }
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
+        .center {
+            position: absolute;
+            width: 390px;
+            height: 480px;
+            margin-top: -210px;
+            margin-left: -240px;
+            top: 50%;
+            left: 50%;
+            text-align: left;
+        }
 
-    <div>
-        Password
-        <input type="password" name="password" id="password">
-    </div>
+    </style>
+@stop
+@section("body")
+<div class="center">
+    <div class="card blue-grey darken-1">
+        <form method="POST" action="/auth/login">
+            <div class="card-content white-text">
+                <img src="/images/mark.png" alt="" height="60px">
+                <div class="card-title">你準備起飛了嗎？</div>
+                {!! csrf_field() !!}
 
-    <div>
-        <input type="checkbox" name="remember"> Remember Me
-    </div>
+                <div>
+                    Email
+                    <input type="email" name="email" value="{{ old('email') }}">
+                </div>
 
-    <div>
-        <button type="submit">Login</button>
+                <div>
+                    密碼
+                    <input type="password" name="password" id="password">
+                </div>
+
+                <div>
+                    <input type="checkbox" name="remember" id="remember"> 
+                    <label for="remember">記住我</label>
+                </div>
+            </div>
+            <div class="card-action">
+                <div>
+                    <button type="submit" class="btn cyan darken-2">登入</button>
+                </div>
+            </div>
+        </form>
     </div>
-</form>
+</div>
+@stop
