4354e7ecd781311336bc8841b8ebb33cfe50e024
LarryStanley <kill.xmurderer@gmail.com>
2016-02-26 10:30:17 +0800

    Add explode animation in products view.

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -6,87 +6,101 @@
 @stop
 
 @section("body")
-	@include("nav")
 	<div class="main">
-		<div id="ark90Cover">
-			<div class="valign-wrapper" style="width:100%; height: 100%;">
-				<div class="container">
-					<div class="valign center-align" style="text-align: center;">
-						<h1>奶油蒼蠅 3.0</h1>
-						<p style="font-size: 20px; z-index: 10;">讓 Arduino 飛行，一直都是這麼容易</p>
-						<img src="http://i.imgur.com/jXqgae8.png" alt="" width="45%"><br>	
-						<a href="#details" class="btn red darken-2 waves-effect">瞭解更多</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div id="ark90Details">
-			<div class="valign-wrapper" style="width:100%;">
-				<div class="container">
-					<p class="center-align valign">
-						<span style="font-size: 36px;">讓你的Arduino 起飛 ・ 奶油蒼蠅3.0 </span><br>
-						全新的奶油蒼蠅做了硬體整合 <br>
-						插拔核心部份再一次進化 <br>
-						Ark NANO 與 Arduino NANO 由於角位設計相同 <br>
-						使擁有 Arduino nano 的玩家也可以無痛接軌來操控奶油蒼蠅 <br>
-						擴充性高也代表有更多新的玩法<br>
-					</p>
-					<div class="row">
-						<div class="col m4 s12 center-align">
-							<h3>ArkNano</h3>
-							<img src="/images/arknano.png" alt="" width="120px">
-							<ul>
-								<li>九軸感測器</li>
-								<li>主核心芯片 ATmega328p-AU</li>
-								<li>九軸感測器芯片</li>
-								<li>藍芽通訊</li>
-							</ul>
-						</div>
-						<div class="col m4 s12 center-align">
-							<h3>ArkNano Pro</h3>
-							<img src="/images/arknanopro.png" alt="" width="120px">
-							<ul>
-								<li>九軸 + 氣壓高度感測器</li>
-								<li>主核心芯片 ATmega328p-AU</li>
-								<li>九軸感測器芯片</li>
-								<li>藍芽通訊</li>
-							</ul>
-						</div>
-						<div class="col m4 s12 center-align">
-							<h3 style="font-size: 28px">ArkNano Professional</h3>
-							<img src="/images/arknanoprofessional.png" alt="" width="120px">
-							<ul>
-								<li>九軸 + 氣壓高度感測器</li>
-								<li>主核心芯片 STM32</li>
-								<li>九軸感測器芯片</li>
-								<li>藍芽通訊</li>
-							</ul>
+		@include("nav")
+		<section style="padding: 0 0 0 0">
+			<div id="ark90Cover">
+				<div class="valign-wrapper" style="width:100%; height: 100%;">
+					<div class="container">
+						<div class="valign center-align" style="text-align: center;">
+							<h1>奶油蒼蠅 3.0</h1>
+							<p style="font-size: 20px; z-index: 10;">讓 Arduino 飛行，一直都是這麼容易</p>
+							<img src="http://i.imgur.com/jXqgae8.png" alt="" width="45%"><br>	
+							<a href="#details" class="btn red darken-2 waves-effect">瞭解更多</a>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-		<div id="arkEduCover">
-			<div class="valign-wrapper page-mask">
-				<div class="container">
-					<div class="valign center-align">
-						<p style="font-size: 36px; margin-bottom: 5px;">
-							奶油蒼蠅<br>讓你我都是自造者
-						</p>
-						<a href="/education" class="btn waves-effect cyan darken-2">
-							工作坊內容
-						</a>
-					</div>				
+		</section>
+		<section style="padding: 0 0 0 0">
+			<div id="ark90Explode">
+				<div class="valign-wrapper" style="width:100%; height: 100%;">
+					<div class="container">
+						<div class="valign">
+							<div class="row">
+								<div class="col m6">
+									<img src="http://i.imgur.com/d3noOA0.png" id="prop" alt="" style="position:absolute; z-index: 6;" >
+									<img src="http://i.imgur.com/ZJKOJVF.png" id="frame" alt="" style="position:absolute; z-index: 5">
+									<img src="http://i.imgur.com/H5Tfg0r.png" id="arduino" alt="" style="position:absolute; z-index: 4">
+									<img src="http://i.imgur.com/CD8X2Zs.png" id="quad" alt="" style="position:absolute; z-index: 3">	
+									.
+								</div>
+								<div class="col m6">
+									<h3>可插拔核心</h3>
+									<p style="font-size: 20px">機身可更換，豐富多變化</p>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
+		</section>
+		<section style="padding: 0 0 0 0">
+			<div id="arkEduCover">
+				<div class="valign-wrapper page-mask">
+					<div class="container">
+						<div class="valign center-align">
+							<p style="font-size: 36px; margin-bottom: 5px;">
+								奶油蒼蠅<br>讓你我都是自造者
+							</p>
+							<a href="/education" class="btn waves-effect cyan darken-2">
+								工作坊內容
+							</a>
+						</div>				
+					</div>
+				</div>
+			</div>
+		</section>
 	</div>
 	@include("footer")
 
 	@section("javascript")
 		<script src="/js/jquery.onepage-scroll.min.js"></script>
 		<script>
-
+			$(".main").onepage_scroll({
+			   sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+			   easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+			                                    // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+			   animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+			   pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+			   updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+			   beforeMove: function(index) {
+			   	if (index == 2) {
+			   		var objects = ["#prop", "#frame", "#arduino", "#quad"];
+			   		$.each(objects, function(key, object) {
+			   			$(object).animate({
+				   			marginTop: 0
+				   		});
+			   		});
+			   	}
+			   },  // This option accepts a callback function. The function will be called before the page moves.
+			   afterMove: function(index) {
+			   	if (index == 2) {
+			   		var objects = ["#prop", "#frame", "#arduino", "#quad"];
+			   		$.each(objects, function(key, object) {
+			   			$(object).animate({
+				   			marginTop: -200 + key * 100
+				   		}, 2000);
+			   		});
+			   	}
+			   },   // This option accepts a callback function. The function will be called after the page moves.
+			   loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+			   keyboard: true,                  // You can activate the keyboard controls
+			   responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+			                                    // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+			                                    // the browser's width is less than 600, the fallback will kick in.
+			   direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+			});
 		</script>
 	@stop
 @stop
