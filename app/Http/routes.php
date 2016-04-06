d91a6a6b693dc391ebd4187ce42e48d6e1e675c3
LarryStanley <kill.xmurderer@gmail.com>
2016-04-06 11:36:26 +0800

    Block register.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -34,8 +34,8 @@ Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
 Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@postRegister');
+//Route::get('auth/register', 'Auth\AuthController@getRegister');
+//Route::post('auth/register', 'Auth\AuthController@postRegister');
 
 Route::get('/home', function() {
 	return redirect('/dashboard');
